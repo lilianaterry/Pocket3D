03a0e64f15bdc8e37188576af93d138b1c314945
Chris Day <chrisday0001@gmail.com>
2019-02-26 17:11:13 -0600

    Initial Commit

---
+++ Pocket3D/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  Pocket3D
+//
+//  Created by Chris Day on 2/26/19.
+//  Copyright © 2019 Team 2. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
